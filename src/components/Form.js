fe34c6e26e012f5a57a5bb8005ef0c2e49b065ad
karafaze <fazli.carrat@gmail.com>
2023-02-10 19:21:47 +0100

    fixed form for mobile and tablets

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { send } from "emailjs-com";
 
-export default function Form() {
+export default function Form2() {
     const [formData, setFormData] = useState({
         from_name: "",
         to_name: "Fazli",
@@ -39,74 +39,83 @@ export default function Form() {
 
     return (
         <React.Fragment>
-            <section id="form" className="form--container">
-            <p className="form--message">Please don't hesitate to send me a message if you're looking for a web developer, insights or simply discuss anything regarding web programming ! </p>
-            <form onSubmit={handleSubmit} className="form">
-                <div className="form--left">
-                    <div className="form--item">
-                        <label className="form--item__label" htmlFor="name">
-                            Name
-                        </label>
-                        <input
-                            type="text"
-                            name="from_name"
-                            id="name"
-                            placeholder="John Doe"
-                            value={formData.from_name}
-                            onChange={handleChange}
-                            className="form--item__input"
-                        />
+            <section id="form" className="form">
+                <p className="form--title">
+                    Please don't hesitate to send me a message if you're looking
+                    for a web developer, insights or simply discuss anything
+                    regarding web programming !{" "}
+                </p>
+                <form onSubmit={handleSubmit} className="form--container">
+                    <div className="form--left">
+                        <div className="form--item">
+                            <label className="form--item__label" htmlFor="name">
+                                Name
+                            </label>
+                            <input
+                                type="text"
+                                name="from_name"
+                                id="name"
+                                placeholder="John Doe"
+                                value={formData.from_name}
+                                onChange={handleChange}
+                                className="form--item__input"
+                            />
+                        </div>
+                        <div className="form--item">
+                            <label
+                                className="form--item__label"
+                                htmlFor="email"
+                            >
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="johndoe@email.com"
+                                value={formData.email}
+                                onChange={handleChange}
+                                className="form--item__input"
+                            />
+                        </div>
+                        <div className="form--item">
+                            <label className="form--item__label" htmlFor="from">
+                                How did you come by here ?
+                            </label>
+                            <input
+                                type="text"
+                                name="from"
+                                id="from"
+                                placeholder="LinkedIn, Random, Ads..."
+                                value={formData.from}
+                                onChange={handleChange}
+                                className="form--item__input"
+                            />
+                        </div>
                     </div>
-                    <div className="form--item">
-                        <label className="form--item__label" htmlFor="email">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            placeholder="johndoe@email.com"
-                            value={formData.email}
-                            onChange={handleChange}
-                            className="form--item__input"
-                        />
+                    <div className="form--right">
+                        <div className="form--item">
+                            <label
+                                className="form--item__label"
+                                htmlFor="message"
+                            >
+                                Message
+                            </label>
+                            <textarea
+                                name="message"
+                                id="message"
+                                placeholder="Tell me a good story"
+                                value={formData.message}
+                                onChange={handleChange}
+                                className="form--item__textarea"
+                            />
+                        </div>
+                        <button className="form--btn" type="submit">
+                            Send mail
+                        </button>
                     </div>
-                    <div className="form--item">
-                        <label className="form--item__label" htmlFor="from">
-                            How did you end up here ?
-                        </label>
-                        <input
-                            type="text"
-                            name="from"
-                            id="from"
-                            placeholder="LinkedIn, Random, Ads..."
-                            value={formData.from}
-                            onChange={handleChange}
-                            className="form--item__input"
-                        />
-                    </div>
-                </div>
-                <div className="form--right">
-                    <div className="form--item">
-                        <label className="form--item__label" htmlFor="message">
-                            Message
-                        </label>
-                        <textarea
-                            name="message"
-                            id="message"
-                            placeholder="Tell me a good story"
-                            value={formData.message}
-                            onChange={handleChange}
-                            className="form--item__textarea"
-                        />
-                    </div>
-                    <button className="form--btn" type="submit">
-                        Send mail
-                    </button>
-                </div>
-            </form>
+                </form>
             </section>
-
         </React.Fragment>
     );
 }
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Presentation() {
     return (
         <React.Fragment>
-            <p className="homepage--presentation">
+            <div className="presentation--container">
+                <p className="presentation--content">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
                 ullamcorper, ante at interdum convallis, libero massa luctus mi,
                 nec semper erat lectus vel urna. Phasellus sodales, lorem sed
@@ -11,7 +12,23 @@ export default function Presentation() {
                 id sem. Ut finibus est id nibh malesuada maximus. Nullam egestas
                 mauris dictum vehicula hendrerit. Nunc rutrum dignissim ex
                 convallis faucibus.
-            </p>
+                </p>
+            </div>
+            {/* <div className="presentation--container">
+                <p className="presentation--content">
+                    I used to be a sale and then I discovered programming !
+                </p>
+                <p className="presentation--content">
+                    Quit my job and started learning Python on my own : webscrapping, web with Django, a bit of data with Panda
+                </p>
+                <p className="presentation--content">
+                    Started a course in may 2022 to learn Web Development with OpenClassrooms. Ended with a certification and new competencies !
+                </p>
+                <p className="presentation--content">
+                    Highly methodical web developer with competencies in both Front and Back End. 
+                    I enjoy creating beautiful web apps with clean code and user friendly interfaces. 
+                </p>
+            </div> */}
         </React.Fragment>
     );
 }
